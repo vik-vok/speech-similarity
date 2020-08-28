17b399e489c17f262b1a68392f7511a08c6ccc16
nikalosa <nikalosaberidze@gmail.com>
2020-08-28 18:15:46 +0400

    Bigquery insert recorded voice row

--- function/main.py
+++ function/main.py
@@ -1,12 +1,15 @@
 import base64
 import json
+import random
 
 from google.cloud import datastore
+from google.cloud import bigquery
 
 
 project_id = 'speech-similarity'
 datastore_client = datastore.Client(project_id)
-
+client = bigquery.Client()
+table_id = 'speech-similarity:statistics.recorded_voices'
 
 def compare_voices(event, context):
     if event.get('data'):
@@ -18,11 +21,22 @@ def compare_voices(event, context):
     print(message)
 
     dic = dict(message)
-    dic['score'] = .5
+    dic['score'] = random.uniform(.5, .99)
 
-    with datastore_client.transaction():
-        incomplete_key = datastore_client.key('Statistics')
-        user = datastore.Entity(key=incomplete_key)
-        user.update(dic)
-        datastore_client.put(user)
+
+    rows = [(dic['parentId'], dic['userId'], '2016-05-19T10:38:47.046465')]
+
+    table = client.get_table(table_id)
+    errors = client.insert_rows(table, rows)
+
+    if errors == []:
+        print("New recorded voice added successfully")
+    else:
+        print(errors)
+
+    # with datastore_client.transaction():
+    #     incomplete_key = datastore_client.key('Statistics')
+    #     user = datastore.Entity(key=incomplete_key)
+    #     user.update(dic)
+    #     datastore_client.put(user)
 
